b79152a6e5e8ef6c22c7d48dd323d9f83d8f9efe
Sami Mokaddem <mokaddem.sami@gmail.com>
2024-07-04 11:23:35 +0200

    chg: [front:socketConnectionState] Small UI improvement for disconnected socket

--- src/components/TheSocketConnectionState.vue
+++ src/components/TheSocketConnectionState.vue
@@ -13,13 +13,16 @@
     <span :class="{
       'px-2 rounded-md inline-block w-48 leading-4': true,
       'text-slate-900 dark:text-slate-400': socketConnected,
-      'text-slate-50 bg-red-600': !socketConnected,
+      'text-slate-50 bg-red-600 px-2 py-1': !socketConnected,
     }">
       <span class="mr-1">Socket.IO:</span>
       <span v-show="socketConnected" class="font-semibold text-green-600 dark:text-green-400">Connected</span>
       <span v-show="!socketConnected" class="font-semibold text-slate-50">Disconnected</span>
     </span>
-    <span class="text-xs font-thin leading-3 inline-block text-center">
+    <span
+      v-if="socketConnected"
+      class="text-xs font-thin leading-3 inline-block text-center"
+    >
       <template v-if="zmqLastTimeSecond == 0">
         online
       </template>
