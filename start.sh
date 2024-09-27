b3ce19a6e90c356fb8dc47afd2fd31bcfdea518c
Sami Mokaddem <mokaddem.sami@gmail.com>
2024-09-27 14:59:50 +0200

    fix: [docker] Fixed docker image build and added misp access env variables

--- start.sh
+++ start.sh
@@ -4,13 +4,16 @@ DEFAULT_HOST="127.0.0.1"
 DEFAULT_PORT="4001"
 
 usage() {
-    echo "Usage: $0 --exercise_folder <folder> [--host <host>] [--port <port>]"
+    echo "Usage: $0 --exercise_folder <folder> [--host <host>] [--port <port>] [--misp_url <misp_url>] [--misp_apikey <misp_apikey>] [--misp_skipssl <misp_skipssl>]"
     exit 1
 }
 
 HOST="${SKILLAEGIS_HOST:-""}"
 PORT="${SKILLAEGIS_PORT:-""}"
 EXERCISE_FOLDER="${SKILLAEGIS_EXERCISE_FOLDER:-""}"
+MISP_URL="${SKILLAEGIS_MISP_URL:-""}"
+MISP_APIKEY="${SKILLAEGIS_MISP_APIKEY:-""}"
+MISP_SKIPSSL="${SKILLAEGIS_MISP_SKIPSSL:-""}"
 
 while [ "$#" -gt 0 ]; do
     case "$1" in
@@ -24,6 +27,18 @@ while [ "$#" -gt 0 ]; do
             ;;
         --exercise_folder)
             EXERCISE_FOLDER="$2"
+            shift 2
+            ;;
+        --misp_url)
+            MISP_URL="$2"
+            shift 2
+            ;;
+        --misp_apikey)
+            MISP_APIKEY="$2"
+            shift 2
+            ;;
+        --misp_skipssl)
+            MISP_SKIPSSL="$2"
             shift 2
             ;;
         *)
@@ -41,4 +56,9 @@ HOST=${HOST:-$DEFAULT_HOST}
 PORT=${PORT:-$DEFAULT_PORT}
 
 source venv/bin/activate
-python3 ./backend/main.py --host "$HOST" --port "$PORT" --exercise_folder "$EXERCISE_FOLDER"
+python3 ./backend/main.py \
+    --host "$HOST" --port "$PORT" \
+    --exercise_folder "$EXERCISE_FOLDER" \
+    --misp_url "$MISP_URL" \
+    --misp_apikey "$MISP_APIKEY" \
+    --misp_skipssl "$MISP_SKIPSSL"
